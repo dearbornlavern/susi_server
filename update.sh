080a8355daae6c97c92ba29caf2035642aaf1f41
Norbert Preining <norbert@preining.info>
2020-05-24 19:19:04 +0900

    add comments about git commit

--- update.sh
+++ update.sh
@@ -7,3 +7,11 @@ mv susi_server_binary_latest/{bin,build,conf,html,system-integration} .
 rmdir susi_server_binary_latest/release
 rmdir susi_server_binary_latest/
 rm susi_server_binary_latest.tar.gz
+
+# now do
+# - check for new files that **should** be included and do
+#     git add .....
+#   for them
+# - then do git commit --amend 
+# - and git push --force origin dev-dist  (or stable-dist)
+
