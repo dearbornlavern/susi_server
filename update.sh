ea8b2f9023e59264877873d001d483c1fa2bff4e
Michael Christen <mc@acy.net>
2020-06-08 10:23:37 +0300

    wqadded ssi

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 set -e
 wget http://download.susi.ai/susi_server/susi_server_binary_latest.tar.gz
 tar -xzf susi_server_binary_latest.tar.gz
-rm -rf bin build conf html system-integration
-mv susi_server_binary_latest/{bin,build,conf,html,system-integration} .
+rm -rf bin ssi build conf html system-integration
+mv susi_server_binary_latest/{bin,ssi,build,conf,html,system-integration} .
 rmdir susi_server_binary_latest/release
 rmdir susi_server_binary_latest/
 rm susi_server_binary_latest.tar.gz
